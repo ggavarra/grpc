086d14aa33daac6726f9f46b37dc2edd757e02dd
Gayathri Gavarraju <gayathri1.g@gmail.com>
2021-03-17 06:16:59 +0800

    First cut

---
+++ src/main/java/com/example/grpc/GrpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class GrpcApplication {
+
+	public static void main(String[] args) {
+		
+	}
+}
+
